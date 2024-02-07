58f0e213112a584a9f532311ce597f60091cec55
Abdullah Şahin <99759896+AbdullahSahin1@users.noreply.github.com>
2024-02-07 23:28:27 +0300

    Update script.js

--- NetflixLoginPage/script.js
+++ NetflixLoginPage/script.js
@@ -1,4 +1,35 @@
-const btn = document.querySelector('.btn');
-btn.addEventListener("click",e=>{
-    window.open();
+const netfix = document.querySelector('.netfix');
+const p = document.querySelector('.netflixp')
+netfix.addEventListener('click',()=>{
+    document.querySelector(".netflixp").style.display = "block"
+})
+
+const cost = document.querySelector('.cost');
+const costP = document.querySelector('.costp')
+cost.addEventListener('click',()=>{
+    document.querySelector(".costp").style.display = "block"
+})
+
+const where = document.querySelector('.where');
+const whereP = document.querySelector('.wherep')
+where.addEventListener('click',()=>{
+    document.querySelector(".wherep").style.display = "block"
+})
+
+const kids = document.querySelector('.kids');
+const kidsp = document.querySelector('.kidsp')
+kids.addEventListener('click',()=>{
+    document.querySelector(".kidsp").style.display = "block"
+})
+
+const what = document.querySelector('.what');
+const whatp = document.querySelector('.whatp')
+what.addEventListener('click',()=>{
+    document.querySelector(".whatp").style.display = "block"
+})
+
+const ignore = document.querySelector('.ignore');
+const ignorep = document.querySelector('.ignorep')
+ignore.addEventListener('click',()=>{
+    document.querySelector(".ignorep").style.display = "block"
 })
